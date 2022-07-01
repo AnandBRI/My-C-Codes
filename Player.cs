f0781a4f83b592f2d866648330e1ec42b1056f41
AnandBRI <104568878+AnandBRI@users.noreply.github.com>
2022-07-01 12:32:33 +0530

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon;
 using UnityEngine.UI;
+using Photon;
+using Photon.Pun;
 
-public class Player : Photon.PunBehaviour
+
+public class Player : MonoBehaviourPun, IPunObservable
 {
     public MPManager mp;
     public Text healthText;
@@ -18,17 +20,22 @@ public class Player : Photon.PunBehaviour
     public Vector3 camOffset;
     private float x;
     private float z;
+    private Animator anim;
+    private PlayFabAuth pauth;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
-        if (photonView.isMine)
+        anim = GetComponent<Animator>();
+        if (photonView.IsMine)
         {
+
+            pauth = GameObject.FindGameObjectWithTag("PlayFab").GetComponent<PlayFabAuth>();
+
             userText.text = username;
         }
         else
         {
-            
+
         }
     }
 
@@ -36,36 +43,50 @@ public class Player : Photon.PunBehaviour
     {
         if (collision.gameObject.tag == "Hurt")
         {
-            if (photonView.isMine)
+            if (photonView.IsMine)
             {
-                photonView.RPC("Damage", PhotonTargets.All);
+                photonView.RPC("Damage", RpcTarget.All);
             }
+        }
+        if (collision.gameObject.tag == "End")
+        {
+            pauth.SetWins(1);
+            mp.SetWinner(username);
         }
     }
     [PunRPC]
     void Damage()
     {
-        Health -= 20;
+        Health -= 30;
     }
+  
+ 
     void Update()
     {
-        if (photonView.isMine)
+        if (photonView.IsMine)
         {
             if (!mp.gameStart)
                 return;
+            if (mp.aPlayerHasWon)
+                return;
             x = Input.GetAxisRaw("Horizontal");
             z = Input.GetAxisRaw("Vertical");
+            float move = x + z;
+            anim.SetFloat("hop", move);
             Vector3 camPos = Camera.main.transform.position;
             Vector3 pPos = transform.position;
             Camera.main.transform.position = Vector3.Lerp(camPos, new Vector3(pPos.x, pPos.y, pPos.z) + camOffset, 0.4f);
-
+         
             if (Health > MaxHealth)
             {
                 Health = MaxHealth;
             }
             if (Health < MinHealth)
             {
-                Health = MinHealth;
+                Vector3 pos = mp.spawnPoints[Random.Range(0, mp.spawnPoints.Count)].transform.position;
+                transform.position = pos;
+                Health = MaxHealth;
+              //  Health = MinHealth;
             }
         }
         else
@@ -76,7 +97,7 @@ public class Player : Photon.PunBehaviour
     private void FixedUpdate()
     {
         healthText.text = Health.ToString();
-        if (photonView.isMine)
+        if (photonView.IsMine)
         {
             rb.AddForce(new Vector3(x, 0, z) * Speed);
         }
@@ -84,15 +105,16 @@ public class Player : Photon.PunBehaviour
 
     public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.isWriting)
+        if (stream.IsWriting)
         {
             stream.SendNext(Health);
             stream.SendNext(username);
         }
-        else if (stream.isReading)
+        else if (stream.IsReading)
         {
             Health = (float)stream.ReceiveNext();
             username = (string)stream.ReceiveNext();
+            userText.text = username;
         }
     }
 }
